c2c2b4999dcf9b5e22edb763764b9d69cb4c8570
Atsushi Morimoto <74th.tech@gmail.com>
2025-10-25 16:22:01 +0900

    up

--- 20251025-root_write/tasks.py
+++ 20251025-root_write/tasks.py
@@ -3,6 +3,7 @@ import time
 from invoke.context import Context
 from invoke.tasks import task
 
+
 def get_local_uid_gid(c: Context) -> tuple[int, int]:
     r = c.run("id -u")
     assert r
@@ -12,79 +13,128 @@ def get_local_uid_gid(c: Context) -> tuple[int, int]:
     local_gid = int(r.stdout.strip())
     return local_uid, local_gid
 
-def write(c: Context, target_name: str, docker_command: str="docker"):
-    c.run(f"{docker_command} run --rm -v $(pwd):/workspace -e TARGET=tmp/{target_name}.txt -w /workspace debian bash test_write.sh")
 
-def test(c: Context, target_name: str, docker_command: str="docker", compose_command: str="docker compose"):
-    c.run("sudo rm -rf tmp/*.txt", warn=True)
-
-    local_uid, local_gid = get_local_uid_gid(c)
-
-    write(c, target_name, docker_command=docker_command)
-
-    stat = os.stat(f"tmp/{target_name}.txt")
-
-    if stat.st_uid != local_uid:
-        print(f"- Test1: ❌ Error UID mismatch! local_uid={local_uid} uid={stat.st_uid}")
-    else:
-        print(f"- Test1: ✅ used same UID: local_uid={local_uid} uid={stat.st_uid}")
+def write(c: Context, target_name: str, docker_command: str = "docker"):
+    c.run(
+        f"{docker_command} run --rm -v $(pwd):/workspace -e TARGET=tmp/{target_name}.txt -w /workspace debian bash test_write.sh"
+    )
 
 
+def test(
+    c: Context,
+    target_name: str,
+    docker_command: str = "docker",
+    compose_command: str = "docker compose",
+    run_only: str | None = None,
+):
+    _run_only = run_only if run_only is None else int(run_only)
 
-    c.run(f"{docker_command} run --rm -d -p 8080:80 --name nginx-test nginx:latest")
+    if _run_only is None or _run_only == 1:
+        c.run("sudo rm -rf tmp/*.txt", warn=True)
 
-    time.sleep(2)
+        local_uid, local_gid = get_local_uid_gid(c)
 
-    r = c.run("curl -s http://localhost:8080", warn=True)
-    assert r
-    if r.ok:
-        print("- Test2: ✅ can access nginx by localhost:8080")
-    else:
-        print("- Test2: ❌ Error: cannot access nginx by localhost:8080")
+        write(c, target_name, docker_command=docker_command)
 
-    c.run(f"{docker_command} rm -f nginx-test")
+        stat = os.stat(f"tmp/{target_name}.txt")
 
+        if stat.st_uid != local_uid:
+            print(
+                f"- Test1: ❌ Error UID mismatch! local_uid={local_uid} uid={stat.st_uid}"
+            )
+        else:
+            print(f"- Test1: ✅ used same UID: local_uid={local_uid} uid={stat.st_uid}")
 
-    c.run(f"{compose_command} up -d")
+    if _run_only is None or _run_only == 2:
+        c.run(f"{docker_command} run -d -p 8080:80 --name nginx-test nginx:latest")
 
-    time.sleep(2)
+        time.sleep(2)
 
-    r = c.run("curl -s http://localhost:8080", warn=True)
-    assert r
-    if r.ok:
-        print("- Test3: ✅ can access nginx by http://localhost:8080")
-    else:
-        print("- Test3: ❌ Error: cannot access nginx by http://localhost:8080")
+        r = c.run("curl -s http://localhost:8080")
+        assert r
+        if r.ok:
+            print("- Test2: ✅ can access nginx by localhost:8080")
+        else:
+            print("- Test2: ❌ Error: cannot access nginx by localhost:8080")
 
-    r = c.run(f"{compose_command} exec -it dev curl -s http://nginx", warn=True)
-    assert r
-    if r.ok:
-        print("- Test4: ✅ can access nginx by http://nginx inside dev container")
-    else:
-        print("- Test4: ❌ Error: cannot access nginx by http://nginx inside dev container")
+        c.run(f"{docker_command} rm -f nginx-test")
 
-    c.run(f"{compose_command} down")
-    c.run(f"{compose_command} rm")
+    if _run_only is None or _run_only in (3, 4):
+        c.run(f"{compose_command} up -d")
+
+        time.sleep(2)
+
+        if _run_only is None or _run_only == 3:
+            r = c.run("curl -s http://localhost:8080")
+            assert r
+            if r.ok:
+                print("- Test3: ✅ can access nginx by http://localhost:8080")
+            else:
+                print("- Test3: ❌ Error: cannot access nginx by http://localhost:8080")
+
+        if _run_only is None or _run_only == 4:
+            r = c.run(f"{compose_command} exec -it dev curl -s http://nginx")
+            assert r
+            if r.ok:
+                print(
+                    "- Test4: ✅ can access nginx by http://nginx inside dev container"
+                )
+            else:
+                print(
+                    "- Test4: ❌ Error: cannot access nginx by http://nginx inside dev container"
+                )
+
+        c.run(f"{compose_command} down")
+        c.run(f"{compose_command} rm -y")
+
+    if _run_only is None or _run_only == 5:
+        r = c.run(f"{docker_command} run --rm --platform linux/amd64 debian uname -a")
+        assert r
+        if r.ok and r.stdout.count("x86_64"):
+            print("- Test5: ✅ can run amd64 container")
+        else:
+            print("- Test5: ❌ Error: cannot run amd64 container")
 
 
 @task
-def run_docker_desktop(c: Context):
+def run_docker_desktop(c: Context, run_only: str | None = None):
     c.run("docker context use desktop-linux")
 
-    test(c, "docker-desktop")
+    test(c, "docker-desktop", run_only=run_only)
+
 
 @task
-def run_podman(c: Context):
-    test(c, "podman", docker_command="podman", compose_command="PODMAN_COMPOSE_PROVIDER=podman-compose podman compose")
+def run_podman(c: Context, run_only: str | None = None):
+    test(
+        c,
+        "podman",
+        docker_command="podman",
+        compose_command="PODMAN_COMPOSE_PROVIDER=podman-compose podman compose",
+        run_only=run_only,
+    )
+
 
 @task
-def run_rancher_desktop(c: Context):
+def run_rancher_desktop(c: Context, run_only: str | None = None):
     c.run("docker context use rancher-desktop")
 
-    test(c, "rancher-desktop")
+    test(c, "rancher-desktop", run_only=run_only)
+
 
 @task
-def run_colima(c: Context):
+def run_colima(c: Context, run_only: str | None = None):
     c.run("docker context use colima")
 
-    test(c, "rancher-desktop")
+    test(c, "collima", run_only=run_only)
+
+
+@task
+def run_finch(c: Context, run_only: str | None = None):
+    print(f"runonly: {run_only}")
+    test(
+        c,
+        "finch",
+        docker_command="finch",
+        compose_command="finch compose",
+        run_only=run_only,
+    )
--- 20251025-root_write/test_write.sh
+++ 20251025-root_write/test_write.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+set -ex
 echo hoge > ./${TARGET}
+chown `id -u`:`id -g` ./${TARGET}
+ls -al ./tmp/
